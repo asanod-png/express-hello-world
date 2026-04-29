cc3c8989087e28712b80772c5419830729d4389f
asanod-png <asano.d@gmail.com>
2026-04-29 15:13:29 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import line from "@line/bot-sdk";
+
+const config = {
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+};
+
+const app = express();
+app.use(express.json());
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+async function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  const client = new line.Client(config);
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: `あなたは「${event.message.text}」と送りましたね。`
+  });
+}
+
+app.listen(10000, () => {
+  console.log("LINE Bot is running!");
+});
